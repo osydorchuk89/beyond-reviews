fd5f469c36322f62136177313eb127a502a5fb07
Oleksii Sydorchuk <oleksii.sydorchuk@gmail.com>
2024-05-24 10:59:14 +0200

    small fixes

--- backend/dist/util/urls.js
+++ backend/dist/util/urls.js
@@ -2,9 +2,9 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.BASE_CLIENT_URL = exports.BASE_API_URL = exports.BASE_URL = void 0;
 exports.BASE_URL = process.env.NODE_ENV === "production"
-    ? "https://beyond-reviews.onrender.com"
+    ? "https://beyond-reviews.onrender.com/"
     : "http://localhost:3000/";
 exports.BASE_API_URL = exports.BASE_URL + "api/";
 exports.BASE_CLIENT_URL = process.env.NODE_ENV === "production"
-    ? "https://beyond-reviews-smoc.onrender.com/"
+    ? "https://beyond-reviews-smoc.onrender.com"
     : "http://localhost:5173";
