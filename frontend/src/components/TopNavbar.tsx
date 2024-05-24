fc8a3a19a990ea70f2d987246cd3215567971994
Oleksii Sydorchuk <oleksii.sydorchuk@gmail.com>
2024-05-24 15:29:00 +0200

    small fix

--- frontend/src/components/TopNavbar.tsx
+++ frontend/src/components/TopNavbar.tsx
@@ -42,7 +42,7 @@ export const TopNavBar = () => {
     useQuery({ queryKey: ["users"], queryFn: getUsers });
 
     const isAuthenticated = authStatus!.isAuthenticated;
-    console.log(authStatus);
+    // console.log(authStatus);
 
     const router = useRouterState();
     const currentPath = router.location.pathname;
