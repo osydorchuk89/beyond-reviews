e4291b1f1cf62ce31233c9ebf34fdaa657c948a7
Oleksii Sydorchuk <oleksii.sydorchuk@gmail.com>
2025-05-03 07:22:16 +0200

    edited session options

--- server/src/app.ts
+++ server/src/app.ts
@@ -34,6 +34,12 @@ app.use(
         secret: process.env.EXPRESS_SESSION_SECRET!,
         resave: false,
         saveUninitialized: false,
+        cookie: {
+            secure: process.env.NODE_ENV === "production" ? true : false,
+            sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+            // httpOnly: true,
+            // maxAge: 1000 * 60 * 24,
+        },
     })
 );
 app.use(passport.initialize());
