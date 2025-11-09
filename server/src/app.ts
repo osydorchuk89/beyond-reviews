6e642933571d61cd462da8f77b76dbe194cee7f8
Oleksii Sydorchuk <oleksii.sydorchuk@gmail.com>
2025-11-09 21:45:51 +0100

    revert "refactor server port configuration"
    This reverts commit acce7c1ad8d229ff2b7bba8acfd02e47421b6905.

--- server/src/app.ts
+++ server/src/app.ts
@@ -60,8 +60,4 @@ app.get("/", (_, res) => {
     res.send("Hello World!!!");
 });
 
-const PORT = parseInt(process.env.PORT || '8080', 10);
-
-app.listen(PORT, '0.0.0.0', () => {
-    console.log(`Server listening on port ${PORT}`);
-});
+app.listen(process.env.PORT || 8080);
